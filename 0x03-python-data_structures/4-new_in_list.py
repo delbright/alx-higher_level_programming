dd23ce4004437f1c468623d050cb01e341725963
Aime Fabrice <aimefabrigas@gmail.com>
2022-09-26 13:34:10 +0200

    new item in a list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-
-
 def new_in_list(my_list, idx, element):
-
-    if (idx < 0) or (idx > (len(my_list)-1)):
-        return my_list
-
-    copy = [x for x in my_list]
-    copy[idx] = element
-    return copy
+    list_2 = my_list[:]
+    if idx >= 0 and idx < len(list_2):
+        list_2[idx] = element
+    return list_2
